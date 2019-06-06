4619c704bcdd096bc61a77e2460f98cadcf56685
tongwenqian <839035680@qq.com>
2019-06-06 19:22:18 +0800

    提交

--- main.js
+++ main.js
@@ -4,36 +4,26 @@ let $images = $slides.children('img')
 let current = 0
 
 makeFakeSlides()
-$slides.css({transform: 'translateX(-400px)'})
+$slides.css({transform:'translateX(-400px)'})
 bindEvents()
-$(next).on('click',function () {
+$(next).on('click',function() {
     goToSlide(current+1)
 })
-$(previous).on('click',function () {
+$(previous).on('click',function() {
     goToSlide(current-1)
 })
 
-let timer = setInterval(function () {
+let timer = setInterval(function(){
     goToSlide(current+1)
 },2000)
 
-$('.container').on('mouseenter',function () {
+$('.container').on('mouseenter',function(){
     window.clearInterval(timer)
-}).on('mouseleave',function () {
-    timer = setInterval(function () {
+}).on('mouseleave',function(){
+    timer = setInterval(function(){
         goToSlide(current+1)
     },2000)
 })
-
-
-//调用
-function makeFakeSlides(){
-    let $firstCopy = $images.eq(0).clone(true)
-    let $lastCopy = $images.eq($images.length-1).clone(true)
-
-    $slides.append($firstCopy)
-    $slides.prepend($lastCopy)
-}
 
 function bindEvents(){
     $('#buttonWrapper').on('click','button',function(e){
@@ -51,12 +41,13 @@ function goToSlide(index) {
     }
     if (current === $buttons.length-1 && index === 0){
         //最后一张到第一张
-        $slides.css({transform: `translateX(${($buttons.length+1)*400}px)`})
+        $slides.css({transform: `translateX(${-($buttons.length+1)*400}px)`})
             .one('transitionend',function(){
-                $slides.hide().offset()
+                $slides.hide()
+                $slides.offset()
                 $slides.css({transform: `translateX(${-(index+1)*400}px)`}).show()
             })
-    }else if(current === 0 && $buttons.length-1){
+    }else if(current === 0 && index === $buttons.length-1){
         //从第一张到最后一张
         $slides.css({transform: 'translateX(0)'})
             .one('transitionend',function(){
@@ -67,4 +58,12 @@ function goToSlide(index) {
         $slides.css({transform: `translateX(${-(index+1)*400}px)`})
     }
     current = index
+}
+
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
 }
